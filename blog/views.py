d03f1eaf5e14969412fe54ee0568512f8f7c190a
subh1685 <shubhamsngh067@gmail.com>
2017-08-22 18:11:40 +0530

    changed html

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,7 @@ from django.utils import timezone
 
 def post_list(request):
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    post1=Post.objects.filter(title="Where to start from??")
+    post2=Post.objects.filter(title="after implementations..")
+    
+    return render(request, 'blog/post_list.html', {'postss':posts})
